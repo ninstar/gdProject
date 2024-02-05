3b5944ee689061d235ed8dac0a71c66670995520
Nin★ <ninstar@pm.me>
2024-02-05 16:37:00 -0300

    Fixed base class types

--- addons/nine_patch_sprite_2d/plugin.gd
+++ addons/nine_patch_sprite_2d/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_custom_type("NinePatchSprite2D", "NinePatchSprite2D", preload("nine_patch_sprite_2d.gd"), preload("nine_patch_sprite_2d.svg"))
+	add_custom_type("NinePatchSprite2D", "Node2D", preload("nine_patch_sprite_2d.gd"), preload("nine_patch_sprite_2d.svg"))
 
 
 func _exit_tree():
--- addons/resource_overrider/plugin.gd
+++ addons/resource_overrider/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_custom_type("ResourceOverrider", "ResourceOverrider", preload("resource_overrider.gd"), preload("resource_overrider.svg"))
+	add_custom_type("ResourceOverrider", "Node", preload("resource_overrider.gd"), preload("resource_overrider.svg"))
 
 
 func _exit_tree():
